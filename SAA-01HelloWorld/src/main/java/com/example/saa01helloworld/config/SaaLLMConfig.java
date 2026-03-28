03e0fe829f52194d50c4ca3590115f9dc132b1b2
chenzhixin <1327921893@qq.com>
2026-03-28 20:18:48 +0800

    初始化项目配置，提交到github仓库

---
+++ SAA-01HelloWorld/src/main/java/com/example/saa01helloworld/config/SaaLLMConfig.java
@@ -0,0 +1,26 @@
+package com.example.saa01helloworld.config;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SaaLLMConfig {
+
+
+    @Value("${spring.ai.dashscope.api-key}")
+    private String apiKey;
+
+
+    @Bean
+    public DashScopeApi dashScopeApi(){
+        return DashScopeApi.builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+
+
+
+}
